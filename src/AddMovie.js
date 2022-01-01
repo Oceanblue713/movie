6c711555a10c534b013b76747ff4b8863f74612d
Anri Liy <amedamachip713@gmail.com>
2022-01-01 11:58:02 -0800

    AddMovie

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
+import { MovieContext } from './MovieContext';
 
 const AddMovie = () => {
 
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
+  const [movies, setMovies] = useContext(MovieContext)
 
   const updateName = (e) => {
     setName(e.target.value);
@@ -15,13 +17,16 @@ const AddMovie = () => {
 
   const addMovie = (e) => {
     e.preventDefault();
+    setMovies(prevMovies => [...prevMovies, {name: name, price: price}])
   }
 
   return (
-    <form>
+    <form onSubmit={addMovie}>
       <input type="text" name="name" value={name} onChange={updateName} />
       <input type="text" name="price" value={price} onChange={updatePrice} />
       <button>Submit</button>
     </form>
   )
 }
+
+export default AddMovie;
--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import MovieList from './MovieList';
 import Nav from './Nav';
 import { MovieProvider } from './MovieContext';
+import AddMovie from './AddMovie';
 
 function App() {
   return (
     <MovieProvider>
       <div className="App">
+        <AddMovie />
         <Nav />
         <MovieList />
       </div>
